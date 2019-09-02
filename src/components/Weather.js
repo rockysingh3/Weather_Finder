e1312ab6ef771d0bf088e1b67fbaeba2fd755659
Rocky Singh <rockygsingh@icloud.com>
2019-09-02 16:53:54 -0400

    rendered the state and its values

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,11 @@ export default class Weather extends React.Component {
     render() {
         return (
             <div>
-                <h1>This is the Weather component</h1>
+                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>  }
+                { this.props.temp &&  <p>Teamperature: {this.props.temp}</p>}
+                { this.props.description && <p> Description: {this.props.description}</p>}
+                { this.props.humidity && <p> Humidity: {this.props.humidity}</p>}   
+                <p>{this.props.error}</p>
             </div>
         )
     }
