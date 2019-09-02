d0b954a0b01d7dd3c6864e3d5d8fcbb30023ae68
Rocky Singh <rockygsingh@icloud.com>
2019-09-02 16:52:53 -0400

    passed the state down to Weather.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import KEY from './API_KEY';
 
 export default class App extends React.Component {
 
+  state = {
+    temp: '',
+    city: '',
+    description: '',
+    country: '',
+    humidity: '',
+    error: ''
+  }
+
+
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value
@@ -17,6 +27,15 @@ export default class App extends React.Component {
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${KEY.api_key}`)
     const data = await api_call.json()
     console.log(data)
+
+    this.setState({
+      temp: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    });
   }
 
   render() {
@@ -24,7 +43,14 @@ export default class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather 
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description }
+          error={this.state.error}
+        />
       </div>
     )
   }
