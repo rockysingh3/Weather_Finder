d7f761e37bba9fce997ed1ea6733e7ac7b788557
Rocky Singh <rockygsingh@icloud.com>
2019-09-02 18:12:53 -0400

    included all weather info

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,12 +3,29 @@ import React from 'react';
 
 const Weather = props => {
         return (
-            <div>
-                { props.city && props.country && <p>Location: { props.city }, { props.country }</p>  }
-                { props.temp &&  <p>Teamperature: { props.temp }</p>}
-                { props.description && <p> Description: { props.description }</p>}
-                { props.humidity && <p> Humidity: { props.humidity }</p>}
-                { props.error && <p>{ props.error }</p>}
+            <div className="weather_info">
+                { 
+                    props.city && props.country && <p className="weather__key">Location: 
+                    <span className="weather__value">{ props.city }, { props.country }</span></p>  
+                }
+                { 
+                    props.temp &&  <p className="weather__key">Teamperature: 
+                    <span className="weather__value">{ props.temp }</span>
+                    </p>
+                }
+                { 
+                    props.description && <p className="weather__key"> Description: 
+                    <span className="weather__value">{ props.description }</span>
+                    </p>
+                }
+                { 
+                    props.humidity && <p className="weather__key"> Humidity: 
+                    <span className="weather__value">{ props.humidity }</span>
+                    </p>
+                }
+                { 
+                    props.error && <p className="weather__error">{ props.error }</p>
+                }
             </div>
         )
 }
